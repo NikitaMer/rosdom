f02bee8ed8e23c672d8f6b484dadfff98e92af42
Admin <git@webgk.ru>
2017-08-10 15:47:37 +0300

    local changes 10.08.2017

--- faq/success.php
+++
@@ -1,4 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Вопрос успешно добавлен");
-?>Ваш вопрос добавлен<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
